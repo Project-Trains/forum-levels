31f7e42e27021133cae049a7dae76fb2f380f3c5
kalucky0 <kalucki23@gmail.com>
2021-02-07 00:50:00 +0100

    Update to flarum beta 15

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,5 @@
 import app from 'flarum/app';
-import ForumLevelsSettingsModal from './components/ForumLevelsSettingsModal';
+import ForumLevelsSettingsModal from './components/LevelRanksSettingsModal';
 
 app.initializers.add('reflar-level-ranks', () => {
     app.extensionSettings['reflar-level-ranks'] = () => app.modal.show(new ForumLevelsSettingsModal());
